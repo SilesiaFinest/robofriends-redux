5ba9b486961cc57adfbcb5f0c1fae4ac3688cae9
Zbigniew Baranski <zb.baranski@icloud.com>
2019-10-17 20:17:58 +0200

    connect() done in app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
-//first declare states
+// import action for redux
+import { setSearchField } from '../actions';
+
+//define functions to be used by connect(), this is default syntax to be used in future!
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+//state declaration, searchfield already deleted
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
 //use DiDMount to fetch users data from web and pass it to the robots > then to filter and CardList
@@ -19,15 +34,15 @@ class App extends Component {
             .then(response => response.json())
             .then(users => this.setState({ robots: users}));
     }
-// event received from SearchBox.js being passed to searchfield state
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    }
+// onSearchChange method deleted as it's done by connect() functions
+
 //render, deconstruction, then create filteredRobots from searchfield input
+// searchField and onSearchChange passed to props by mapStateToProps
     render () {
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
 // if robots.lenght=0 show Loading. Classes by tachyons. Searchbox prop value is passed
 // CardList receives filteredRobots list as prop, wrapped in error boundry
@@ -36,7 +51,7 @@ class App extends Component {
             (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filteredRobots} />
@@ -46,4 +61,5 @@ class App extends Component {
             );
     };
 }
-export default App;
+// using higher order function connect() for redux, this is correct syntax
+export default connect(mapStateToProps, mapDispatchToProps)(App);
